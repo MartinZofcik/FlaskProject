80a2443dc4c8c3fcbc530cbcefdef8847ed49af6
Martin Žofčík <martinzof1@gmail.com>
2020-07-09 16:46:26 +0000

    Added info page

--- app.py
+++ app.py
@@ -4,5 +4,11 @@ from flask import render_template
 flask_app = Flask(__name__)
 
 @flask_app.route("/")
-def index():
+def view_index():
    return render_template("index.jinja")
+
+
+@flask_app.route("/info/")
+def view_info():
+    text = "Toto je info"
+    return render_template("info.jinja", text = text)
